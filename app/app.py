5fa5b3673d8395622c70c006a12be3d9f6b2067d
mostafaeslam <mostafaeslam1220@gmail.com>
2024-10-23 22:41:51 +0300

    Refactor paths and add error handling in app.py

--- app/app.py
+++ app/app.py
@@ -1,20 +1,21 @@
 import os
 import numpy as np
 import cv2
-from tensorflow.keras.models import model_from_json
+from keras.models import model_from_json
 from flask import Flask, request, render_template, redirect, url_for, jsonify
-from tensorflow.keras.mixed_precision import set_global_policy
-
-
+from keras.mixed_precision import set_global_policy
 
 # Initialize Flask app
 app = Flask(__name__)
 
 # Define paths to files
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_JSON_PATH = os.path.join(BASE_DIR, 'model.json')
-MODEL_H5_PATH = os.path.join(BASE_DIR, 'model.h5')
-CASCADE_PATH = os.path.join(BASE_DIR, 'haarcascade_frontalface_default.xml')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MODEL_DIR = os.path.join(BASE_DIR, 'models')
+MODEL_JSON_PATH = os.path.join(MODEL_DIR, 'model.json')
+MODEL_H5_PATH = os.path.join(MODEL_DIR, 'model.h5')
+CASCADE_PATH = os.path.join(MODEL_DIR, 'haarcascade_frontalface_default.xml')
+STATIC_DIR = os.path.join(BASE_DIR, 'app', 'static')
+first_time = True
 
 # Load the model architecture and weights
 set_global_policy('float32')
@@ -31,19 +32,27 @@ face_cascade = cv2.CascadeClassifier(CASCADE_PATH)
 # Route for homepage with image upload
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    
+    global first_time
+    if first_time:
+        first_time = False
+        return render_template('index.html', emotion="Upload an image")
+
+    app.logger.info('Processing image...')
+
     if request.method == 'POST':
         # Save uploaded image
         file = request.files['image']
         if file:
-            file_path = os.path.join('static', 'output.jpg')
+            file_path = os.path.join(STATIC_DIR, 'output.jpg')
             file.save(file_path)
 
             # Process the uploaded image
             emo = process_image(file_path)
             if emo:
-                return render_template("index.html", mass=emo) 
+                return render_template("index.html", emotion=emo) 
             else:
-                return  render_template("index.html", mass="Error processing image") 
+                return  render_template("index.html", emotion="Error processing image") 
 
     return render_template('index.html')
 
@@ -55,18 +64,14 @@ def process_image(image_path):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         # Detect faces using Haar cascade
-        
-        
         label = None
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         
         if len(faces) !=1:
             return label
-        app.logger.info(f"Detected {len(faces)} faces in image")
         
         face = faces[0]
         (x, y, w, h) = face
-
 
         # Predict emotion for each detected face
         face = gray[y:y + h, x:x + w]
@@ -76,19 +81,15 @@ def process_image(image_path):
         # Predict emotion using the model
         prediction = model.predict(face)
         emotion = np.argmax(prediction)
-        emotion_detection = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral') # list of emotions
+        emotion_detection = ('Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral') # list of emotions
 
-        # Annotate image with predicted emotion
-        label = f'Emotion: {emotion_detection[emotion]}'
-
-        # Save the processed image
-        app.logger.info(f"Emotion detected: {label}")
-        return label
+        # Return the detected emotion
+        return emotion_detection[emotion]
 
     except Exception as e:
-        print(f"Error processing image: {e}")
+        print(f"Error processing image: {e}") 
         return False
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     
